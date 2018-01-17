b728a1c470659e49e00819829c7bcaff21e25a0e
Lucas Riutzel <lriutzel@gmail.com>
2018-01-17 13:59:33 -0600

    Issue 1 responsive breaks cleanup

--- web/themes/custom/goddard_uswds_sub/gulpfile.js
+++ web/themes/custom/goddard_uswds_sub/gulpfile.js
@@ -1,17 +1,31 @@
 'use strict';
- 
+
 var gulp = require('gulp');
-var	sass = require('gulp-sass');
-var	watch = require('gulp-watch');
- 
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
+
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+var scssSrc = './assets/styles/scss';
+
+
 gulp.task('sass', function () {
-  return gulp.src('assets/styles/scss/*.scss')
+  var processors = [
+    autoprefixer({browsers: ['last 2 versions']}),
+    cssnano()
+  ];
+  return gulp.src(scssSrc + '/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('assets/styles/css'));
 });
- 
+
 gulp.task('watch', function () {
-  gulp.watch('assets/styles/scss/*.scss', ['sass']);
+  gulp.watch(scssSrc + '/**/*.scss', ['sass']);
 });
 
 gulp.task('default');
