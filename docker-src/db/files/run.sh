8660a078b9d2007bf6afd5edd74f7768640183ce
Lucas Riutzel <lucas@mobomo.com>
2019-05-22 10:28:28 -0500

    new mariadb doesn't set correct datadir or something

--- docker-src/db/files/run.sh
+++ docker-src/db/files/run.sh
@@ -25,7 +25,7 @@ else
 
 	chown -R mysql:mysql /var/lib/mysql
 
-	mysql_install_db --user=mysql > /dev/null
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
 	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
 		MYSQL_ROOT_PASSWORD=`pwgen 16 1`
